9d05934ddb921a37c7c6baa96ff3c7e355851e7e
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-26 16:28:36 +0000

    Updated through rs script

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,68 @@
 /* routes hér */
 const express = require('express');
+const pgp = require('pg-promise')();
 
 const router = express.Router();
-const dbop = require('./db.js')
+const env = process.env.DATABASE_URL;
+const db = pgp(env);
+
+
+
+// sækir þræði af gerðinni sub. page fyrir blaðsíðu númer.
+function getSub(sub, page) {
+  db.any(); // select, where sub=sub.
+  return;
+}
+
+// sækir þráðin með kommentum þessarar blaðsíðu
+// function getThread(threadID, page) {
+function getThread(threadID) {
+    // select, faum fyrsta innleggið
+  db.one('SELECT * FROM thread WHERE id = $1', threadID)
+    .then((thread) => {
+      // fáum öll kommentin. innan við page.
+      db.any('SELECT * FROM thread WHERE threadID = $1', threadID)
+        .then ((comments) => {
+          res.render('index', {
+            thread: thread,
+            comments: comments,
+          });
+          }
+        })
+    }
+}
+
+// nýr þráður er búinn til.
+function newThread(title, name, date, sub, paragraph) {
+  db.none()  // insert og svo viljum við fá þráðin
+  .then(() => {
+    // þurfum að searcha ID.
+    getThread(ID, 0);  // faum þráðinn og bls 0 for now.
+    // success;
+  })
+  .catch((error) => {
+    // error;
+  });
+  return;
+}
+
+// nýtt komment er búið til
+function newComment(name, date, threadID, paragraph) {
+  db.none()  // insert, svo viljum við fá þráðin
+  .then(() => {
+    getThread(threadID, 0);  // faum þráðinn og bls 0 for now.
+    // success;
+  })
+  .catch((error) => {
+    // error;
+  });
+  return;
+}
+
+// sækir 10 nýlegast modified þræðina.
+function top10() {
+  return;
+}
 
 function index(req, res) {
   res.render('index', {
