f5ea3d535662b0087e03b99e2f133418c94c837a
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-27 16:23:09 +0000

    Updated through rs script

--- src/routes.js
+++ src/routes.js
@@ -12,7 +12,7 @@ const db = pgp(env || 'postgres://postgres:hallgrimur@localhost/test');
 function pageNum(id) {
   let str = 'SELECT COUNT(*) FROM';
   str = str.concat('(SELECT id FROM threads WHERE id = $1 UNION');
-  str = str.concat('(SELECT id FROM comments WHERE threadid = $1) AS derivedTable');
+  str = str.concat('(SELECT id FROM comments WHERE threadid = $1)');
   return db.one(str, id);
 }
 
