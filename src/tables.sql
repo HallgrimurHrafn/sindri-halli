d979ad6473166ec2c147c16a7e9742652b6387e4
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-27 22:31:18 +0000

    Updated through rs script

--- src/tables.sql
+++ src/tables.sql
@@ -15,3 +15,19 @@ CREATE TABLE comments (
   paragraph text,
   date timestamp with time zone not null default current_timestamp
 );
+
+Last edited: skilar dagsetningu þess sem síðast var breytt.
+output.date er dagsetningin
+(SELECT date FROM threads where id = $1 UNION ALL
+SELECT date FROM comments where threadid = $1)
+ORDER BY date desc limit 1;
+
+select distinct threadid FROM
+((SELECT threadid, date from comments order by date desc) UNION
+(SELECT id, date from threads order by date desc)) as test;
+
+
+
+select threads.id, comments.date from threads left outer join comments
+on threads.id = comments.threadid
+order by comments.date desc;
