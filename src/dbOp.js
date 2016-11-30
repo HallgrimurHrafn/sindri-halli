9e4b091e6ca97b38b0fa3c6b162f3e747b7611dd
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-30 16:32:38 +0000

    Updated through rs script

--- src/dbOp.js
+++ src/dbOp.js
@@ -14,6 +14,14 @@ function pageNum(id) {
   return db.one(str, id);
 }
 
+
+function indexx(ord, page) {
+  const str1 = ('SELECT * FROM threads ORDER BY ').concat(ord).concat(' LIMIT $1 offset $2');
+  const str2 = 'SELECT COUNT(*) FROM (SELECT id FROM threads ) AS test';
+  return Promise.all([db.any(str1, [10, (page * 10)]), db.one(str2)]);
+}
+
+
 function getSub(sub, page, ord) {
   // database command 1:
   let str = ('SELECT * FROM threads WHERE sub ilike $1 ORDER BY ');
@@ -23,6 +31,7 @@ function getSub(sub, page, ord) {
   str2 = str2.concat('(SELECT id FROM threads WHERE sub = $1) AS test');
   return Promise.all([db.any(str, [sub, 10, (page * 10)]), db.one(str2, sub)]);
 }
+
 
 function getThread(threadID, page) {
   const str1 = 'SELECT distinct title FROM total WHERE threadid = $1';
@@ -36,15 +45,66 @@ function getThread(threadID, page) {
   ]);
 }
 
-function indexx(ord, page) {
-  const str1 = ('SELECT * FROM threads ORDER BY ').concat(ord).concat(' LIMIT $1 offset $2');
-  const str2 = 'SELECT COUNT(*) FROM (SELECT id FROM threads ) AS test';
-  return Promise.all([db.any(str1, [10, (page * 10)]), db.one(str2)]);
+
+function newThread(name, paragraph, title, sub) {
+  const str = 'insert into threads (name, paragraph, title, sub) values ($1, $2, $3, $4) returning id';
+  return db.one(str, [name, paragraph, title, sub]);
+}
+
+
+function newComment1(name, paragraph, threadID) {
+  const str = 'insert into comments (name, paragraph, threadID) values ($1, $2, $3)';
+  let str2 = '(SELECT date FROM threads where id = $1 UNION ALL ';
+  str2 = str2.concat('SELECT date FROM comments where threadid = $1) ');
+  str2 = str2.concat('ORDER BY date desc limit 1');
+  return Promise.all([
+    db.none(str, [name, paragraph, threadID]),
+    pageNum(threadID),
+    db.one(str2, threadID),
+  ]);
+}
+
+
+function newComment2(results, threadID) {
+  const str3 = 'UPDATE threads SET comnum = $1, mdate = $2 where id=$3';
+  return Promise.all([
+    results,
+    db.none(str3, [results[1].count - 1, results[2].date, threadID]),
+  ]);
+}
+
+
+function searcher(searched, page, type) {
+  let str = ('SELECT * FROM total WHERE ').concat(type);
+  str = str.concat(' @@ to_tsquery($1) ORDER BY date desc');
+  const str2 = str.concat(' LIMIT $2 offset $3');
+  const count = ('SELECT COUNT(*) FROM ( ').concat(str).concat(') AS blah');
+  return Promise.all([
+    db.any(str2, [searched, 10, 10 * page]),
+    db.one(count, searched),
+  ]);
+}
+
+function allsearch(searched, page) {
+  let str = ('SELECT * FROM total WHERE name @@ to_tsquery($1) or ');
+  str = str.concat('title @@ to_tsquery($1) or paragraph @@ to_tsquery($1)');
+  str = str.concat('ORDER BY date desc');
+  const str2 = str.concat(' LIMIT $2 offset $3');
+  const count = ('SELECT COUNT(*) FROM ( ').concat(str).concat(') AS blah');
+  return Promise.all([
+    db.any(str2, [searched, 10, 10 * page]),
+    db.one(count, searched),
+  ]);
 }
 
 module.exports = {
   pageNum,
+  indexx,
   getSub,
   getThread,
-  indexx,
+  newThread,
+  newComment1,
+  newComment2,
+  searcher,
+  allsearch,
 };
--- src/routes.js
+++ src/routes.js
@@ -14,6 +14,13 @@ const db = pgp(env || 'postgres://postgres:hallgrimur@localhost/test');
 
 
 // ef linkurinn er rangur innan marka logum vid.
+// annars sendum vid a forsidu.
+function indexprep(x, req, res) {
+  const url = strOp.indexPrep(x);
+  res.redirect(url);
+}
+
+// ef linkurinn er rangur innan marka logum vid.
 // annars sendum vid a forsidu videigandi category.
 function getSubPrep(x, req, res) {
   const url = strOp.subPrep(x);
@@ -29,20 +36,23 @@ function getThreadPrep(req, res, x) {
 }
 
 
+// NOTE: her koma function til ad directa url einhvert.
+
+
 // setjum baetum vid &page=0 aftan a url.
 function DirectToPage0(req, res) {
   const url = req.url.concat('&page=0');
   res.redirect(url);
 }
 
-
-// ef linkurinn er rangur innan marka logum vid.
-// annars sendum vid a forsidu.
-function indexprep(x, req, res) {
-  const url = strOp.indexPrep(x);
-  res.redirect(url);
+function DirectToSub(req, res) {
+  const sub = req.url.concat('&sort=mdate').concat('&page=0');
+  res.redirect(sub);
 }
 
+function DirectToIndex(req, res) {
+  res.redirect('/sort=mdate&page=0');
+}
 
 // NOTE: Hér byrja get að ferðir.
 
@@ -138,87 +148,78 @@ function index(req, res) {
 }
 
 
-// nýr þráður er búinn til.
-function newThread(req, res) {
-  const title = req.body.title;
-  const name = req.body.name;
-  const paragraph = req.body.paragraph.replace(/\n?\r\n/g, '<br />');
-  const sub = req.body.sub;
-  // insert og svo viljum við fá þráðin
-  const str = 'insert into threads (name, paragraph, title, sub) values ($1, $2, $3, $4) returning id';
-  db.one(str, [name, paragraph, title, sub])
-    .then((data) => {
-    // þurfum að searcha ID.
-      let x = '/threadid=';
-      x = x.concat(data.id);
-      res.redirect(x);
-    // // success;
-    });
-    // .catch((error) => {
-    //   res.render('error', { title: 'oohh shiet', error });
-    // });
-}
+// NOTE: nýtt dót fyrir database
 
-// nýtt komment er búið til
-function newComment(req, res) {
-  const name = req.body.name;
-  const paragraph = req.body.paragraph.replace(/\n?\r\n/g, '<br />');
-  const re = /[=&]/;
-  const x = req.url.split(re);
-  const threadID = x[1];
-
-  const str = 'insert into comments (name, paragraph, threadID) values ($1, $2, $3)';
-  db.none(str, [name, paragraph, threadID])
-    .then(() => {
-      dbOp.pageNum(threadID)
-        .then((comNum) => {
-          let str2 = '(SELECT date FROM threads where id = $1 UNION ALL ';
-          str2 = str2.concat('SELECT date FROM comments where threadid = $1) ');
-          str2 = str2.concat('ORDER BY date desc limit 1');
-          db.one(str2, threadID)
-            .then((mdate) => {
-              db.none('UPDATE threads SET comnum = $1, mdate = $2 where id=$3', [comNum.count - 1, mdate.date, threadID])
-                .then(() => {
-                  res.redirect(req.url);
-                })
-                .catch((error) => { res.render('error', { title: 'oohh shiet', error }); });
-            })
-            .catch((error) => { res.render('error', { title: 'oohh shiet', error }); });
-        })
-        .catch((error) => { res.render('error', { title: 'oohh shiet', error }); });
-    })
-    .catch((error) => { res.render('error', { title: 'oohh shiet', error }); });
-}
-
-function DirectToSub(req, res) {
-  let sub = req.url;
-  sub = sub.concat('&sort=mdate').concat('&page=0');
-  res.redirect(sub);
-}
-
-function DirectToIndex(req, res) {
-  res.redirect('/sort=mdate&page=0');
-}
 
 function createThread(req, res) {
+  // saekjum upplysingar ur url
   const x = req.url;
   let sel = 1;
+  // sel visar til i hvada category notandi var thegar thradur var
+  // buinn til. finnum hvert category-id var.
   sel = strOp.catFix(x, sel);
+  // og sendum a newthread.pug
   res.render('newthread', { sel });
 }
 
 
-function searchName(name, req, res, page) {
+// nýr þráður er búinn til.
+function newThread(req, res) {
+  // saekjum upplysingarnar
+  const title = req.body.title;
+  const name = req.body.name;
+  // skiptum ut gildum svo notandi geti gert nyjar linur i paragraph
+  const paragraph = req.body.paragraph.replace(/\n?\r\n/g, '<br />');
+  const sub = req.body.sub;
+  // database operations
+  dbOp.newThread(name, paragraph, title, sub)
+    .then((data) => {
+    // faum id thradsins ur dbop.newthread og förum þangað.
+      const x = ('/threadid=').concat(data.id);
+      res.redirect(x);
+    })
+    // ef upp kom villa.
+    .catch((error) => { res.render('error', { title: 'oohh shiet', error }); });
+}
+
+// nýtt komment er búið til
+function newComment(req, res) {
+  // saekjum upplysingar ur body
+  const name = req.body.name;
+  // þyðum nyjar linur ur body yfir i það sem pug skilur.
+  const paragraph = req.body.paragraph.replace(/\n?\r\n/g, '<br />');
+  // saekjum upplysingar ur slodinni.
+  const re = /[=&]/;
+  const x = req.url.split(re);
+  const threadID = x[1];
+  // fyrri hluti database vinnslu
+  dbOp.newComment1(name, paragraph, threadID)
+    .then((results1) => {
+      // tilgangslaus lina svo eslint kvarti ekki yfir notkuninni á =>.
+      const results = results1;
+      // sidari hluti database vinnslu, krefst fyrri hluta til ad virka.
+      return dbOp.newComment2(results, threadID);
+    })
+    .then((results) => {
+      // oll innsetning buinn, refreshum sidunni.
+      res.redirect(req.url);
+    })
+    // ef upp kom villa.
+    .catch((error) => { res.render('error', { title: 'oohh shiet', error }); });
+}
+
+
+// NOTE: hér byrja search aðgerðir.
+
+
+function search(name, req, res, page, type) {
   const name1 = decodeURIComponent(name);
   const name2 = strOp.splitter(name1);
   const titill = ('Search: ').concat(name1);
-  const str = 'SELECT * FROM total WHERE name @@ to_tsquery($1) ORDER BY date desc';
-  const count = ('SELECT COUNT(*) FROM ( ').concat(str).concat(') AS blah');
-  const str2 = str.concat(' LIMIT $2 offset $3');
-  Promise.all([db.any(str2, [name2, 10, page * 10]), db.one(count, name2)])
+  dbOp.searcher(name2, page, type)
     .then((results) => {
       const Pnum = Math.floor((results[1].count - 1) / 10) + 1;
-      const info = ('type=name&search=').concat(name).concat('&');
+      const info = ('type=').concat(type).concat('&search=').concat(name).concat('&');
       res.render('search', {
         title: titill,
         searched: name1,
@@ -231,95 +232,27 @@ function searchName(name, req, res, page) {
     .catch((error) => { res.render('error', { title: 'oohh shiet', error }); });
 }
 
-function searchPar(par, req, res, page) {
-  const par1 = decodeURIComponent(par);
-  const par2 = strOp.splitter(par1);
-  const count = 'SELECT COUNT(*) FROM ( ';
-  let str = 'SELECT * FROM total WHERE paragraph @@ to_tsquery($1) ORDER BY date desc';
-  const str2 = str.concat(' LIMIT $2 offset $3');
-  db.any(str2, [par2, 10, 10 * page])
-    .then((results) => {
-      let t = 'Search: ';
-      t = t.concat(par1);
-      str = count.concat(str).concat(') AS blah');
-      db.one(str, par2)
-        .then((p) => {
-          const Pnum = Math.floor((p.count - 1) / 10) + 1;
-          const info = ('type=paragraph&search=').concat(par).concat('&');
-          res.render('search', {
-            title: t,
-            searched: par1,
-            results,
-            page,
-            Pnum,
-            info,
-          });
-        })
-        .catch((error) => { res.render('error', { title: 'oohh shiet', error }); });
-    })
-    .catch((error) => { res.render('error', { title: 'oohh shiet', error }); });
-}
-
-function searchTitle(title, req, res, page) {
-  const title1 = decodeURIComponent(title);
-  const title2 = strOp.splitter(title1);
-  const count = 'SELECT COUNT(*) FROM ( ';
-  let str = 'SELECT * FROM total WHERE title @@ to_tsquery($1) ORDER BY date desc';
-  const str2 = str.concat(' LIMIT $2 offset $3');
-  db.any(str2, [title2, 10, 10 * page])
-    .then((results) => {
-      let t = 'Search: ';
-      t = t.concat(title1);
-      str = count.concat(str).concat(') AS blah');
-      db.one(str, title2)
-        .then((p) => {
-          const Pnum = Math.floor((p.count - 1) / 10) + 1;
-          const info = ('type=title&search=').concat(title).concat('&');
-          res.render('search', {
-            title: t,
-            searched: title1,
-            results,
-            page,
-            Pnum,
-            info,
-          });
-        })
-        .catch((error) => { res.render('error', { title: 'oohh shiet', error }); });
-    })
-    .catch((error) => { res.render('error', { title: 'oohh shiet', error }); });
-}
-
 function searchAll(all, req, res, page) {
   const all1 = decodeURIComponent(all);
   const all2 = strOp.splitter(all1);
-  const count = 'SELECT COUNT(*) FROM ( ';
-  let str = 'SELECT * FROM total WHERE name @@ to_tsquery($1) or ';
-  str = str.concat('title @@ to_tsquery($1) or paragraph @@ to_tsquery($1) ');
-  const str2 = str.concat('ORDER BY date desc LIMIT $2 offset $3');
-  db.any(str2, [all2, 10, 10 * page])
+  const info = ('type=all&search=').concat(all).concat('&');
+  const t = ('Search: ').concat(all1);
+  dbOp.allsearch(all2, page)
     .then((results) => {
-      let t = 'Search: ';
-      t = t.concat(all1);
-      str = count.concat(str).concat(') AS blah');
-      db.one(str, all2)
-        .then((p) => {
-          const Pnum = Math.floor((p.count - 1) / 10) + 1;
-          const info = ('type=all&search=').concat(all).concat('&');
-          res.render('search', {
-            title: t,
-            searched: all1,
-            results,
-            page,
-            Pnum,
-            info,
-          });
-        })
-        .catch((error) => { res.render('error', { title: 'oohh shiet', error }); });
+      const Pnum = Math.floor((results[1].count - 1) / 10) + 1;
+      res.render('search', {
+        title: t,
+        searched: all1,
+        results: results[0],
+        page,
+        Pnum,
+        info,
+      });
     })
     .catch((error) => { res.render('error', { title: 'oohh shiet', error }); });
 }
 
-function search(req, res) {
+function searching(req, res) {
   let type = req.body.type;
   const text = req.body.search;
   let str = '/type=';
@@ -343,12 +276,8 @@ function searchprep(req, res) {
   page = parseInt(page, 10);
   if (isNaN(page)) {
     res.redirect('/');
-  } else if (TYPE === 'NAME') {
-    searchName(searched, req, res, page);
-  } else if (TYPE === 'PARAGRAPH') {
-    searchPar(searched, req, res, page);
-  } else if (TYPE === 'TITLE') {
-    searchTitle(searched, req, res, page);
+  } else if (TYPE === 'NAME' || TYPE === 'PARAGRAPH' || TYPE === 'TITLE') {
+    search(searched, req, res, page, TYPE.toLowerCase());
   } else if (TYPE === 'ALL') {
     searchAll(searched, req, res, page);
   } else {
@@ -364,7 +293,7 @@ router.post('/threadID=*&page=*', newComment);
 router.get('/threadID=*', DirectToPage0);
 router.get('/cat=*&sort=*&page=*', getSub);
 router.get('/cat=*', DirectToSub);
-router.post('/search=*', search);
+router.post('/search=*', searching);
 router.get('/type=*&search=*&page=*', searchprep);
 // VERDUR AD VERA NEDSTUR
 router.get('/*', DirectToIndex);
